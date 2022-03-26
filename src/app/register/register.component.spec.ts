3c5a154f4b8502a3c86fe2a86e7ca79cafd3ec29
ewoutvdb <vdbewout@gmail.com>
2022-03-26 12:21:48 +0100

    fix spec.ts

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './register.component';
 
@@ -8,7 +9,8 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      imports: [ ReactiveFormsModule ]
     })
     .compileComponents();
   });
