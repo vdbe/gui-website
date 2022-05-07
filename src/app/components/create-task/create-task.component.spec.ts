df4503c1549e686cc47f2969a9ef6314bff574b3
ewoutvdb <vdbewout@gmail.com>
2022-05-07 13:58:33 +0200

    fix test warnings

--- src/app/components/create-task/create-task.component.spec.ts
+++ src/app/components/create-task/create-task.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CreateTaskComponent } from './create-task.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -10,7 +13,7 @@ describe('CreateTaskComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule, RouterTestingModule],
+      imports: [HttpClientModule, RouterTestingModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule],
       declarations: [ CreateTaskComponent ]
     })
     .compileComponents();
